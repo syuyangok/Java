2b592911c994060be6a5d68ef6694f57b9e16105
syuyangok <syuyangok@gmail.com>
2017-02-24 16:33:04 -0600

    Update TooLowPollsException.java

--- TooLowPollsException.java
+++ TooLowPollsException.java
@@ -1,8 +1,6 @@
 /**
  * The TooLowPollsException Class is used to build a OutOfMoney exception.  
  * It is a sub class of Exception.  
- * @author Yuyang Sun
- * @Lab time Tue 6:30
  * @version OOL2
  */
 
